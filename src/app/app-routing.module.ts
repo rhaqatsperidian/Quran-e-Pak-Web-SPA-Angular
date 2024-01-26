359840d376e6fe7280214593aef0769569baa45e
Rizwan Saeedi <143165831+rhaqatsperidian@users.noreply.github.com>
2024-01-26 07:07:21 +0500

    refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ReaderViewComponent } from './readerview/readerview.component';
 
-// Import your components for routing
-
 // Define your routes
 const routes: Routes = [
+<<<<<<< Updated upstream
   { path: '', component: AppComponent },
   { path: 'readerview/:surah', component: ReaderViewComponent },
+=======
+  { path: '', component: HomeComponent },  // Default route leading to HomeComponent
+  { path: 'readerview/:surah', component: ReaderViewComponent },  // Route for ReaderViewComponent with a dynamic parameter
+  { path: '**', component: HomeComponent, pathMatch: 'full' },  // Wildcard route, redirects to HomeComponent if no matching route is found
+>>>>>>> Stashed changes
 ];
 
 // Create and export the NgModule
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],  // Configuring the router with the defined routes
+  exports: [RouterModule]  // Exporting the configured router module
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { }  // AppModule uses this AppRoutingModule to configure the application routes
--- src/app/readerview/readerview.component.ts
+++ src/app/readerview/readerview.component.ts
@@ -20,18 +20,37 @@ export class ReaderViewComponent implements OnInit {
       if (surahId !== undefined) {
         // Handle the Surah ID as needed
         console.log('Surah ID:', surahId);
-        this.service.getSurahData(+surahId).subscribe(data => {
-          this.data = data;
 
-          // Extracting the "ar" values from each object in the array excluding the first item
-          const arValues = this.data.slice(1).map((item: any) => item.ar);
+        // standard
+        //   this.service.getSurahData(+surahId).subscribe({
+        //     next: (data) => {
+        //         // ...
+        //     },
+        //     error: (error) => {
+        //         // ...
+        //     }
+        // });
+        // --------------
+        this.service.getSurahData(+surahId).subscribe({
+          next: (data) => {
+            this.data = data;
 
-          // Combining all "ar" values into a single string
-          this.combinedAr = arValues.join('');
+            // Extracting the "ar" values from each object in the array excluding the first item
+            const arValues = this.data.slice(1).map((item: any) => item.ar);
 
+            // Combining all "ar" values into a single string
+            this.combinedAr = arValues.join('');
+          },
+          error: (error) => {
+            // Handle network failure error here
+            console.error('Network failure:', error);
+            alert(error);
+            // You can display an error message or perform other actions as needed
+          }
         });
       }
     });
   }
+
 
 }
